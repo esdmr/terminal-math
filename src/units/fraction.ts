7772fb16d5ed2d1a21cccbda93ebd5b8675acac8
esdmr <esdmr0@gmail.com>
2021-01-17 23:57:04 +0330

    Add root, fraction

---
+++ src/units/fraction.ts
@@ -0,0 +1,32 @@
+import { Box, compileGroup, Group, Unit } from '../defs';
+
+export default class Fraction implements Unit {
+	constructor (
+		readonly numerator: Group,
+		readonly denominator: Group,
+	) {}
+
+	compile (): Box {
+		const num = compileGroup(this.numerator);
+		const den = compileGroup(this.denominator);
+		const width = Math.max(num.width, den.width);
+		const numoff = (width - num.width) / 2 | 0;
+		const denoff = (width - den.width) / 2 | 0;
+
+		return {
+			render: (canvas, x, y) => {
+				num.render(canvas, x + 1 + numoff, y - num.depth - 1);
+				den.render(canvas, x + 1 + denoff, y + den.height);
+
+				for (let i = 0; i < width + 2; i++) {
+					canvas.draw(x + i, y, '─');
+				}
+			},
+			width: width + 2,
+			height: num.height + num.depth + 1,
+			depth: den.height + den.depth,
+			marginLeft: 1,
+			marginRight: 1,
+		};
+	}
+}
---
+++ src/units/root.ts
@@ -0,0 +1,42 @@
+import { Box, compileGroup, Group, Unit } from '../defs';
+
+export default class Root implements Unit {
+	constructor (
+		readonly degree: Group,
+		readonly radicant: Group,
+	) { }
+
+	compile (): Box {
+		const deg = compileGroup(this.degree);
+		const rad = compileGroup(this.radicant);
+		const degskip = Math.max(deg.width, 1);
+
+		return {
+			render: (canvas, x, y) => {
+				// Tail
+				canvas.draw(x + degskip - 1, y + rad.depth, '╲');
+
+				// Side
+				for (let i = 0; i < rad.depth + rad.height + 1; i++) {
+					canvas.draw(x + degskip + i, y + rad.depth - i, '╱');
+				}
+
+				// Top
+				for (let i = 0; i < rad.width + 2; i++) {
+					canvas.draw(x + degskip + rad.depth + rad.height + 1 + i, y - rad.height - 1, '▁');
+				}
+
+				// Radicant
+				rad.render(canvas, x + degskip + rad.depth + rad.height + 2, y);
+
+				// Degree
+				deg.render(canvas, x, y - 1 - deg.depth);
+			},
+			width: degskip + rad.depth + rad.height + rad.width + 3,
+			height: Math.max(1 + deg.depth + deg.height, rad.height + 2),
+			depth: rad.depth,
+			marginLeft: 1,
+			marginRight: 1,
+		};
+	}
+}
