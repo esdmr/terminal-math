b778e0599c96f09a84902d0f2a969679f2974867
esdmr <esdmr0@gmail.com>
2021-01-20 15:20:00 +0330

    Use Tabs

--- src/helper.ts
+++ src/helper.ts
@@ -1,8 +1,8 @@
 export function* zip2<T1, T2> (iter1: Iterator<T1>, iter2: Iterator<T2>): Generator<[T1, T2]> {
-    while (true) {
-        const item1 = iter1.next();
-        const item2 = iter2.next();
-        if (item1.done || item2.done) return;
-        yield [item1.value, item2.value];
-    }
+	while (true) {
+		const item1 = iter1.next();
+		const item2 = iter2.next();
+		if (item1.done || item2.done) return;
+		yield [item1.value, item2.value];
+	}
 }
