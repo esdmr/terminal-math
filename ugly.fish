39c1f87c822d21cfcad4a28bc5d52963d3949679
esdmr <esdmr0@gmail.com>
2021-01-28 22:12:20 +0330

    Remove min dir before minifying

--- ugly.fish
+++ ugly.fish
@@ -1,9 +1,13 @@
 cd build
+command rm -r min
+echo
+
 for i in **.js
+	echo -n \rMinifying: $i
 	mkdir -p min/(dirname $i)
 	pnpx uglifyjs -cm -o min/(dirname $i)/(basename $i .js).js --ecma 8 --rename --source-map --toplevel --warn $i
 end
 
-if test -e unicode.json.gz
-	cp unicode.json.gz min/
-end
+echo
+test -e unicode.json.gz
+and cp unicode.json.gz min/
